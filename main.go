78ebc2e8d3e078b6c29cd65f300c2b447bc4679b
Graham Hay <grahamrhay@gmail.com>
2014-10-18 16:54:58 +0100

    Use a ticker to schedule pings

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	res, err := ping("google.com", 5)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Min: %f ms\n", res.Min)
-	log.Printf("Avg: %f ms\n", res.Avg)
-	log.Printf("Max: %f ms\n", res.Max)
-	log.Printf("Mdev: %f ms\n", res.Mdev)
+	ticker := time.NewTicker(10 * time.Second)
+	go func() {
+		for _ = range ticker.C {
+			log.Println("ping google.com -c 5")
+			res, err := ping("google.com", 5)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Min: %f ms\n", res.Min)
+			log.Printf("Avg: %f ms\n", res.Avg)
+			log.Printf("Max: %f ms\n", res.Max)
+			log.Printf("Mdev: %f ms\n", res.Mdev)
+		}
+	}()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Received signal: %v\n", <-ch)
+	log.Println("Shutting down")
+	ticker.Stop()
 }
