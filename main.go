82992eefa2bc950f95e7981391c6f326d9eb49d2
Graham Hay <grahamrhay@gmail.com>
2014-10-18 16:26:18 +0100

    Separate ping, and return struct

--- main.go
+++ main.go
@@ -2,26 +2,15 @@ package main
 
 import (
 	"log"
-	"os/exec"
-	"strings"
 )
 
 func main() {
-	target := "google.com"
-	count := "10"
-	out, err := exec.Command("ping", "-c", count, target).Output()
+	res, err := ping("google.com", 5)
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Split(string(out), "\n")
-	summary := lines[len(lines)-2]
-	results := strings.Split(strings.TrimRight(strings.TrimSpace(strings.Split(summary, "=")[1]), " ms"), "/")
-	min := results[0]
-	avg := results[1]
-	max := results[2]
-	mdev := results[3]
-	log.Printf("Min: %s\n", min)
-	log.Printf("Avg: %s\n", avg)
-	log.Printf("Max: %s\n", max)
-	log.Printf("Mdev: %s\n", mdev)
+	log.Printf("Min: %f ms\n", res.Min)
+	log.Printf("Avg: %f ms\n", res.Avg)
+	log.Printf("Max: %f ms\n", res.Max)
+	log.Printf("Mdev: %f ms\n", res.Mdev)
 }
---
+++ ping.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type PingResult struct {
+	Min  float64
+	Avg  float64
+	Max  float64
+	Mdev float64
+}
+
+func ping(target string, count int) (*PingResult, error) {
+	out, err := exec.Command("ping", "-c", strconv.Itoa(count), target).Output()
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(out), "\n")
+	summary := lines[len(lines)-2]
+	results := strings.Split(strings.TrimRight(strings.TrimSpace(strings.Split(summary, "=")[1]), " ms"), "/")
+	min, _ := strconv.ParseFloat(results[0], 64)
+	avg, _ := strconv.ParseFloat(results[1], 64)
+	max, _ := strconv.ParseFloat(results[2], 64)
+	mdev, _ := strconv.ParseFloat(results[3], 64)
+	result := &PingResult{Min: min, Avg: avg, Max: max, Mdev: mdev}
+	return result, nil
+}
